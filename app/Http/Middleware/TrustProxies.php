4c793dbad658ad0d0a8c6db610e70f924c96c2b8
caiosantos.dev@outlook.com <seu@email.com>
2024-01-11 02:21:48 -0300

    finalizando

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
@@ -27,6 +27,6 @@ class TrustProxies extends Middleware
     Request::HEADER_X_FORWARDED_PROTO |
     Request::HEADER_X_FORWARDED_AWS_ELB;
     */
-    // protected $proxies = '**';
+    protected $proxies = '**';
     protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
